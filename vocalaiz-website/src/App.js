85274545694bd11363111caf391e7430aa507607
PandawanFr <pandawanfr@gmail.com>
2019-04-06 16:45:03 -0700

    Microphone button

--- vocalaiz-website/src/App.js
+++ vocalaiz-website/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
 import Audio from './components/audio';
+import microphoneImage from './images/microphone.svg';
 
-import microphoneImage from './images/microphone.png';
+const styles = StyleSheet.create({
+  recordButton: {
+    padding: 0,
+    maxWidth: '5rem',
+    borderRadius: '50%',
+    backgroundColor: '#eee'
+  },
+  recordButtonImage: {
+    maxWidth: '100%'
+  }
+});
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <Audio onStream={this._handleStream} onError={() => console.error('Error')} />
-        <img src={microphoneImage} alt='Record' />
+        <button className={css(styles.recordButton)}>
+          <img className={css(styles.recordButtonImage)} src={microphoneImage} alt='Record' />
+        </button>
       </div>
     );
   }
