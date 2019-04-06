79db3b0c192e068ae02b782d7ca66b973ea75611
PandawanFr <pandawanfr@gmail.com>
2019-04-06 11:50:39 -0700

    Format Expo using SemiStandardJS

--- vocalaiz-client/screens/LinksScreen.js
+++ vocalaiz-client/screens/LinksScreen.js
@@ -4,10 +4,10 @@ import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Links'
   };
 
-  render() {
+  render () {
     return (
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
@@ -22,6 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: '#fff'
+  }
 });
