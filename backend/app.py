27dcc1b3b2ff0b867f9a55ce022ca142fea5ea05
sidk2 <skninja6@gmail.com>
2019-04-07 07:17:30 -0700

    h

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def index():
 
 	# save as wav locally
 	testFile = wave.open('test.wav', 'wb')
-	testFile.setparams((2, 2, 8000, 100, 'NONE', None))
+	testFile.setparams((2, 2, 44100, 100, 'NONE', None))
 	testFile.writeframes(binBase)
 	# send to gcloud api
 	# splice wav into word files
