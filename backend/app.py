97980d124c26b7f27438a336a252ecde0b486a5e
KhoomeiK <rohan.pandey@gmail.com>
2019-04-07 05:28:56 -0700

    yeet

--- backend/app.py
+++ backend/app.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 
 @app.route('/prelim', methods=['POST'])
 def index(word):
-	print(request.form.to_dict(flat=False))
+	req = request.form.to_dict(flat=False)
+	print(req)
+	blob = req.recording
 
 	#files = os.listdir("./")
 	#file = files["%s.afp" % word]
 	return "yeet"
-	'''
-	{
-		word1: [0.921, CorrectPronouncedBlob1]
-		word2: [0.394, blob2]
-	}
-	'''
+
+'''
+{[
+	[word1, 0.921, CorrectPronouncedBlob1]
+	[word2, 0.394, blob2]
+]}
+'''
